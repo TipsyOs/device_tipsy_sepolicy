f31997d281da08b68de8615399c3ae7c8954da5b
Prema Chand Alugu <premaca@gmail.com>
2020-08-29 19:53:14 +0000

    Revert "sepolicy: Break livedisplay hal policy into impl independent ones"
    This reverts commit bfec3414d2729784e7dee5a6ae7dd7cb22b1a91f.
    
    Change-Id: Ie7f8fceb5fc1a1f36ba1341482b5b33bac27dcb0

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -11,11 +11,9 @@ endif
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/tipsy/sepolicy/qcom/dynamic \
     device/tipsy/sepolicy/qcom/system \
     device/tipsy/sepolicy/qcom/private
 else
 BOARD_SEPOLICY_DIRS += \
-    device/tipsy/sepolicy/qcom/dynamic \
     device/tipsy/sepolicy/qcom/vendor
 endif
